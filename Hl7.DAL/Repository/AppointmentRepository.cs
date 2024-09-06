4583029ebb387f3a329dd5201bc265793b57f785
M.Z. <zeynalov@gmail.com>
2024-09-07 00:37:53 +0400

    some changes

---
+++ Hl7.DAL/Repository/AppointmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using Hl7.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl7.DAL.Repository;
+public class AppointmentRepository
+{
+    private readonly Hl7DbContext _context;
+
+    public AppointmentRepository(Hl7DbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> AddAppointment(Appointment appointment)
+    {
+        _context.Appointment.Add(appointment); 
+        _
+    }
+}
